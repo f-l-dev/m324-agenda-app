884c458f6982fbc4f3ac9102e495513898cff7d0
Fx64b <90456762+Fx64b@users.noreply.github.com>
2025-02-19 10:25:07 +0100

    remove inefficient code

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Route } from '@angular/router';
-import { AgendaComponent } from './agenda/agenda.component';
-
-export const appRoutes: Route[] = [
-    { path: '', component: AgendaComponent }
-];
